15400d86b91e483e6e972f8e136d6d9ea9365623
Rushil Desai <rushildesai01@gmail.com>
2021-02-04 20:58:35 -0500

    Sign Up page Created, shapes left

---
+++ App/components/textInput.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { TextInput } from 'react-native-paper';
+import {colors} from '../res/colors';
+
+export default class Text_input extends Component {
+    state = {
+        value: '',
+    }
+    getTextStyle = () =>{
+        let bgColor = colors.student.bgColor;
+        let textColor = colors.student.textColor;
+        let border = colors.student.color;
+        if(this.props.teacher){
+            bgColor = colors.teacher.bgColor;
+            textColor = colors.teacher.textColor;
+            border = colors.teacher.color;
+        }
+        return {
+            backgroundColor: bgColor,
+            color: textColor,
+            borderColor: border
+        }
+    }
+    render() {
+        return (
+            <View style={styles.textContainer}>
+                <Text style={[styles.label, this.getTextStyle()]}>
+                    {this.props.label}
+                </Text>
+                <TextInput
+                    style={[styles.inputStyle, this.getTextStyle()]}
+                    onChangeText={text => {
+                            this.props.callback(text, this.props.label);
+                            this.setState({value: text});
+                        }
+                    }
+                    value={this.state.value}
+                    underlineColor="transparent"
+                    selectionColor = {this.props.teacher ? colors.teacher.color : colors.student.color}
+                    underlineColorAndroid="transparent"
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 30,
+        flex: 1,
+        borderRadius: 30,
+    },
+    textContainer: {
+        paddingHorizontal: '10%'
+    },
+    inputContainer: {
+        flex: 4,
+    },
+    buttonContainer: {
+        flex: 3,
+    },
+    label: {
+        fontSize: 15,
+        fontFamily: 'dosis-regular',
+    },
+    textLarge: {
+        fontSize: 25,
+        fontFamily: 'dosis-regular',
+        fontWeight: 'bold',
+    },
+    inputStyle:{
+        backgroundColor: 'white',
+        borderBottomWidth: 7,
+        borderColor: colors.student.color,
+        borderRadius: 7,
+        height: 30,
+        marginBottom: 30,
+    }
+})
---
+++ App/res/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+    student: {
+        color: '#90395B',
+        textColor: 'black',
+        bgColor: 'white',
+    },
+    teacher: {
+        color: '#DBB0C4',
+        textColor: 'white',
+        bgColor: 'black',
+    }
+}
