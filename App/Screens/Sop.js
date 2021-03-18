46f903431788c6db5cf7d4af01af0dd6aae5bb8e
Tanay Naik <macbookair@Tanays-MacBook-Air.local>
2021-03-18 18:31:40 +0530

    Added bottom nav and Lor Dashboard

---
+++ App/Screens/Sop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Sop = () => {
+  return (
+    <View>
+      <Text>SOP</Text>
+    </View>
+  );
+};
+
+export default Sop;
+
+const styles = StyleSheet.create({});
